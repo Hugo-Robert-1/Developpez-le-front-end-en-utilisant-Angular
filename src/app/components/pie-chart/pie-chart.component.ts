bd30de231927f3fc035c9c0a05d46d8730b0cea4
Hugo-Robert-1 <robert.hugo.051@gmail.com>
2025-04-01 14:21:12 +0200

    feat: ajout du composant pie chart et mise en place du dashboard

---
+++ src/app/components/pie-chart/pie-chart.component.ts
@@ -0,0 +1,68 @@
+import { Component, HostListener } from '@angular/core';
+import { Color, NgxChartsModule, ScaleType } from '@swimlane/ngx-charts';
+import { OlympicService } from 'src/app/core/services/olympic.service';
+
+@Component({
+  selector: 'app-pie-chart',
+  standalone: true,
+  imports: [NgxChartsModule],
+  templateUrl: './pie-chart.component.html',
+  styleUrl: './pie-chart.component.scss'
+})
+export class PieChartComponent {
+  data: any[] = [];
+  tooltipX = 0;
+  tooltipY = 0;
+  showTooltip = false;
+  tooltipText = '';
+  countryName = '';
+  medalCount = '';
+
+  colorScheme: Color = {
+    name: 'custom',
+    selectable: true,
+    group: ScaleType.Ordinal,
+    domain: ['#A8385D', '#7AA3E5', '#A27EA8', '#AAE3F5', '#ADCDED', '#A95963']
+  };
+
+  constructor(private olympicService: OlympicService) {}
+
+  ngOnInit(): void {
+    this.olympicService.getOlympicChartData().subscribe(result => {
+      this.data = result;
+    });
+  }
+
+  onDeactivate(): void {
+    this.showTooltip = false;
+  }
+
+  onSelect(data: any): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(event: any, mouseEvent: MouseEvent): void {
+    const tooltipWidth = 100;
+    const tooltipHeight = 50;
+    
+    this.tooltipX = mouseEvent.clientX - tooltipWidth / 2;
+    this.tooltipY = mouseEvent.clientY - tooltipHeight - 15;
+  
+    this.countryName = event.value.name;
+    this.medalCount = `🏅 ${event.value.value}`;
+  
+    this.showTooltip = true;
+  }
+  
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event: MouseEvent): void {
+    if (this.showTooltip) {
+      const tooltipWidth = 100;
+      const tooltipHeight = 50;
+  
+      this.tooltipX = event.clientX - tooltipWidth / 2;
+      this.tooltipY = event.clientY - tooltipHeight - 15;
+    }
+  }
+}
+
--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,23 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Olympic } from '../models/Olympic';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
   private olympicUrl = './assets/mock/olympic.json';
-  private olympics$ = new BehaviorSubject<any>(undefined);
+  private olympics$ = new BehaviorSubject<Olympic[] | null>(null);
 
   constructor(private http: HttpClient) {}
 
   loadInitialData() {
-    return this.http.get<any>(this.olympicUrl).pipe(
+    return this.http.get<Olympic[]>(this.olympicUrl).pipe(
       tap((value) => this.olympics$.next(value)),
       catchError((error, caught) => {
         // TODO: improve error handling
-        console.error(error);
+        console.error('Erreur au cours de la récupération des données', error);
         // can be useful to end loading state and let the user know something went wrong
         this.olympics$.next(null);
         return caught;
@@ -25,7 +26,54 @@ export class OlympicService {
     );
   }
 
-  getOlympics() {
+  getOlympics(): Observable<Olympic[] | null> {
     return this.olympics$.asObservable();
+  }
+
+  countNumberOfJo(): Observable<number> {
+    return this.getOlympics().pipe(
+      map(olympics => {
+        if (!olympics || olympics.length === 0) return 0;
+
+        const uniqueYears = new Set(
+          olympics.reduce((years, olympic) => {
+            return years.concat(olympic.participations.map(p => p.year));
+          }, [] as number[])
+        );
+
+        return uniqueYears.size;
+      })
+    );
+  }
+
+  countNumberOfCountries(): Observable<number> {
+    return this.getOlympics().pipe(
+      map(olympics => {
+        if (!olympics || olympics.length === 0) return 0;
+
+        const uniqueCountry = new Set(
+          olympics.reduce((country, olympic) => {
+            return country.concat(olympic.country);
+          }, [] as string[])
+        );
+
+        return uniqueCountry.size;
+      })
+    );
+  }
+
+  // Formattage des données pour afficher les données sur le diagramme circulaire
+  getOlympicChartData(): Observable<{ name: string; value: number; id: number }[]> {
+    return this.getOlympics().pipe(
+      map(olympics => {
+        if (!olympics) return [];
+        
+        return olympics.map(country => ({
+          name: country.country,
+          value: country.participations.reduce((sum, p) => sum + p.medalsCount, 0), // Somme des médailles
+          id: country.id
+        }));
+      })
+    );
   }
 }
